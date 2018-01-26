767dd5f76e1b33e88574bf28d51b4d8845ea72cd
Sherif <SherifAbdlNaby@gmail.com>
2018-01-26 06:37:56 +0200

    Added Sql script.

---
+++ sql_database_script.sql
@@ -0,0 +1,29 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+-- Database: `default_database`
+CREATE DATABASE `default_database`;
+
+-- Select database
+USE default_database;
+
+
+-- Table structure for table `user`
+CREATE TABLE `user` (
+  `_id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `passwordHash` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+
+-- Indexes for table `user`
+ALTER TABLE `user`
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+COMMIT;
