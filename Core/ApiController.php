09e3fff7c6f8cb31e4f04336d6c82ca43f434ba5
Sherif <SherifAbdlNaby@gmail.com>
2018-02-02 06:03:30 +0200

    Abstract renderFullError

--- Core/ApiController.php
+++ Core/ApiController.php
@@ -3,5 +3,17 @@
 namespace App\Core;
 
 class ApiController extends Controller {
+    /** Render Custom Full Error in API format.
+     * @param $message
+     * @param null $errorStatusCode
+     * @return string
+     */
+    function renderFullError($message, $errorStatusCode = null)
+    {
+        //Send response code via Header.
+        if (is_numeric($errorStatusCode))
+            http_response_code($errorStatusCode);
 
+        return json_encode(['error' => ['code' => $errorStatusCode, 'message' => $message]]);
+    }
 }
--- Core/Controller.php
+++ Core/Controller.php
@@ -20,6 +20,20 @@ abstract class  Controller{
         $this->params = App::getRouter() -> getParams();
     }
 
+    /** Render Custom Full Error in API format.
+     * @param $message
+     * @param null $errorStatusCode
+     * @return string
+     */
+    function renderFullError($message, $errorStatusCode = null)
+    {
+        //Send response code via Header.
+        if (is_numeric($errorStatusCode))
+            http_response_code($errorStatusCode);
+
+        return 'Error Occurred: '.$message;
+    }
+
     /** Redirect User to the given path.
      * @param $path
      * @return null
